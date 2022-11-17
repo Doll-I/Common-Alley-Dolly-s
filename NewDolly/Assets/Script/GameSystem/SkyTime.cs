1e6785564da5f418ac7219dbfe351818e3a6c946
dani <dani2344@sookmyung.ac.kr>
2022-11-17 12:13:15 +0900

    add2

---
+++ NewDolly/Assets/Script/GameSystem/SkyTime.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyTime : MonoBehaviour
+{
+
+    public float upMax = 8.5f;
+    public float currentPosition;
+    float speed = 0.1f;
+
+    void Start()
+    {
+        currentPosition = transform.position.y;
+    }
+
+    void Update()
+    {
+        currentPosition += Time.deltaTime * speed;
+        if (currentPosition >= upMax)
+        {
+            currentPosition = upMax;
+        }
+
+        transform.position = new Vector3(0, currentPosition, 0);
+    }
+}
